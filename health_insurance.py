4502232bdc9c87e1326d7243cceb4cf8eb25b3c6
mcmadmac11 <mcmanus.dbm@gmail.com>
2015-11-11 16:36:21 -0600

    initial commit

---
+++ health_insurance.py
@@ -0,0 +1,35 @@
+﻿
+class healthPlan():
+  def __init__(self):
+  
+  def get_deductible(self):
+    get_deductible = (input('Enter Your Annual Deductible:$'))
+    deduct = get_deductible
+    return deduct
+  
+  def get_procedureCost(self):
+    opCost = input('Enter the expected cost of your procedure:$')
+    return opCost
+
+  def get_coinsurance(self):
+    coinsurance = input(')
+
+
+
+
+
+
+
+
+    
+hdhp=healthPlan()
+print('this plan is a high deductible health plan')
+hdhp.get_deductible()
+hdhp.get_procedureCost()
+
+
+
+
+
+
+
